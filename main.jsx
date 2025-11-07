43d7e26276e5be0fe19e860445cfa94193b041c5
creative-sparrow <ramona@creativesparrow.uk>
2025-11-07 10:46:06 +0000

    Initialize React application in mAdd main.jsx to mount App componentain.jsx

---
+++ main.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./amazon_image_renamer_drag_rename_demo.jsx";
+
+const root = document.getElementById('root');
+ReactDOM.render(<App />, root);
